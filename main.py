5dd12689733bbf4e9512361f53591e7c0b35670c
Joy <joyglory213@gmail.com>
2023-12-17 20:27:41 +0300

    Routes

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ app = FastAPI()
 def index():
     return  {"Message": "welcome to my first api"}
 
-@app.post('/')
-def create():
+@app.get('/cakes')
+def cakes():
+    return []
+
+@app.get('/cakes/{cake_id}')
+def cake():
+    return []
+
+@app.post('/cakes')
+def create_cake():
     return  {"Message": "posting"}
+
+@app.patch('/cakes/{cake_id}')
+def update_cake(cake_id: int):
+    return  {"Message":f"cakes {cake_id} created succeesfully"}
+
+@app.delete('/cakes/{cake_id}')
+def delete_cake(cake_id: int):
+    return  {"Message":f"cakes {cake_id} deleted succeesfully"}
 
