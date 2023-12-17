ae10edec5fa84fe1831650678f50a8f0c6556a5f
Joy <joyglory213@gmail.com>
2023-12-18 00:58:52 +0300

    crud functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Response
 from schemas import CakeSchema
 from sqlalchemy.orm import Session
 from models import Cake
@@ -11,24 +11,33 @@ def index():
 
 @app.get('/cakes')
 def cakes():
-    return []
+    return cakes
 
 @app.get('/cakes/{cake_id}')
-def cake(db: Session = Depends(get_db)):
-    cakes = db.query(cake).all()
+def cake(cake_id: int, db: Session = Depends(get_db)):
+    cakes = db.query(Cake).filter(Cake.id == cake_id).first()
     return cakes
     
 
-@app.post('/cakes')
-def create_cake(cakes : CakeSchema):
-    print(cakes)
-    return  {"Message": "posting"}
+def create_cake(cake: CakeSchema, db: Session = Depends(get_db)):
+    new_cake = Cake(**cake.model_dump())
 
-@app.patch('/cakes/{cake_id}')
-def update_cake(cake_id: int):
-    return  {"Message":f"cakes {cake_id} created succeesfully"}
+    db.add(new_cake)
+    db.commit()
+    db.refresh(new_cake)
+    return {"message": "cake created successfully", "event": new_cake}
+
 
 @app.delete('/cakes/{cake_id}')
-def delete_cake(cake_id: int):
-    return  {"Message":f"cakes {cake_id} deleted succeesfully"}
+def delete_cake(cake_id: int, db: Session = Depends(get_db)):
+    delete_cake = db.query(cake).filter(Cake.id == cake_id).first()
+
+    if delete_cake == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Event {cake_id} does not exist")
+    else:
+        delete_cake.delete()
+        db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
