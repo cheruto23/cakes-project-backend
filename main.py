571d1e486874e2c2e074ca19558497029007597b
Joy <joyglory213@gmail.com>
2023-12-18 01:51:20 +0300

    setting up middleware

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException, status, Response
+from fastapi.middleware.cors import CORSMiddleware
 from schemas import CakeSchema
 from sqlalchemy.orm import Session
 from models import Cake
 from database import get_db
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                    allow_credentials=True,
+                    allow_methods=["*"],
+                    allow_headers=["*"],
+    )
 
 @app.get('/')
 def index():
